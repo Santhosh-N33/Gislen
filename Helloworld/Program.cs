1582b878dcb3c01e48222a4ce70c25c60b2561a4
santhoshn <santhoshn@TRNE-SANTHOSHN>
2025-04-22 13:09:46 +0530

    Add project files.

---
+++ Helloworld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace HelloWorldConsoleApp
+{
+    class App
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World !");
+            Console.ReadKey();
+        }
+    }
+}
